94e40232e17d62547ce48d811a167ec6c09f8aab
Amadeux17 <alfredoloyocarias02@gmail.com>
2023-07-26 21:53:35 -0600

    base sistema

---
+++ sistema/receptoria/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('usuarios', views.usuarios, name='usuarios'),
+    path('roles', views.roles, name='roles'),
+    path('cobros', views.cobros, name='cobros')
+]
---
+++ sistema/receptoria/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'base.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def cobros(request):
+    return render(request, 'cobros/cobros.html')
+
+def usuarios(request):
+    return render(request, 'administracion/usuarios.html')
+
+def roles(request):
+    return render(request, 'administracion/roles.html')
